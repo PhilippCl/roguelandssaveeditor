19050a1c4e31fcd4315f78b8cd8f65a3eaac7abe
Phil <Phil@PhilPC>
2016-07-23 20:08:06 +0200

    Added icon to primaryStage

--- RogueLandsSaveEditor/src/com/thatbit/rlsaveeditor/RoguelandsSaveEditor.java
+++ RogueLandsSaveEditor/src/com/thatbit/rlsaveeditor/RoguelandsSaveEditor.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class RoguelandsSaveEditor extends Application{
@@ -33,6 +34,7 @@ public class RoguelandsSaveEditor extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("images/icon.png")));
 		Parent root = FXMLLoader.load(getClass().getResource("gui/view/TitleBar.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
